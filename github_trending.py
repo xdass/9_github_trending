302538da757ae5b386cb8ee06f07e954869f9eff
xdass <420600@gmail.com>
2017-10-29 22:31:17 +0500

    Init commit

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,32 @@
+import requests
+import datetime
 
 
-def get_trending_repositories(top_size):
-    pass
+# get template https://api.github.com/search/repositories?q=created:%3E=2017-10-22+language:python&sort=stars&order=desc
+
+def get_trending_repositories(repos, top_size=20):
+    return repos[::top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
+    # https://api.github.com/repos/<owner-name>/<repo-name>/issues
     pass
 
-if __name__ == '__main__':
+
+def print_repository_info():
     pass
+
+
+def get_repositories():
+    week_delta = datetime.timedelta(days=7)
+    week_ago = datetime.date.today() - week_delta
+    params = {'q': 'created:>={}'.format(week_ago), 'sort': 'stars', 'order': 'desc'}
+    r = requests.get('https://api.github.com/search/repositories', params=params)
+    return r.json()['items']
+
+if __name__ == '__main__':
+    repositories_list = get_repositories()
+    trending_repos = get_trending_repositories(repositories_list)
+    print(repositories_list)
+    for repo in repositories_list:
+        print(repo['name'])
