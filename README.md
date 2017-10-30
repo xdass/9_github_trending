2cc72d8504b18da2cd7a2000c16951d4dddabaae
xdass <420600@gmail.com>
2017-10-30 21:59:03 +0500

    Refactor code that load repositories data

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Github Trends
 
-[TODO. There will be project description]
+The script find top 20 github repositories and output name, link and amount of issues.
+
+# Install
+
+```bash
+$ pip install -r requirements.txt
+```
+
+# How to use script
+```bash
+$ python github_trending.py
+Название: deepo
+Ссылка: https://github.com/ufoym/deepo
+Issues: 5
+
+Название: progressive_growing_of_gans
+Ссылка: https://github.com/tkarras/progressive_growing_of_gans
+Issues: 2
+.....
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -2,31 +2,31 @@ import requests
 import datetime
 
 
-# get template https://api.github.com/search/repositories?q=created:%3E=2017-10-22+language:python&sort=stars&order=desc
-
 def get_trending_repositories(repos, top_size=20):
-    return repos[::top_size]
+    return repos[:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    # https://api.github.com/repos/<owner-name>/<repo-name>/issues
-    pass
+    link = "https://api.github.com/repos/{}/{}/issues".format(repo_owner, repo_name)
+    response = requests.get(link)
+    all_issues = response.json()
+    return len([issue for issue in all_issues if issue['state'] == 'open'])
 
 
-def print_repository_info():
-    pass
+def print_repository_info(repositories):
+    for repo in repositories:
+        repo_issues = get_open_issues_amount(repo['owner']['login'], repo['name'])
+        print("""Название: {}\nСсылка: {}\nIssues: {}\n""".format(repo['name'], repo['html_url'], repo_issues))
 
 
 def get_repositories():
     week_delta = datetime.timedelta(days=7)
     week_ago = datetime.date.today() - week_delta
     params = {'q': 'created:>={}'.format(week_ago), 'sort': 'stars', 'order': 'desc'}
-    r = requests.get('https://api.github.com/search/repositories', params=params)
-    return r.json()['items']
+    response = requests.get('https://api.github.com/search/repositories', params=params)
+    return response.json()['items']
 
 if __name__ == '__main__':
     repositories_list = get_repositories()
-    trending_repos = get_trending_repositories(repositories_list)
-    print(repositories_list)
-    for repo in repositories_list:
-        print(repo['name'])
+    top_repositories = get_trending_repositories(repositories_list)
+    print_repository_info(top_repositories)
